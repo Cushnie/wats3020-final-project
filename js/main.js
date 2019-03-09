8e9e4b68754f5e88fefcc02ffff0a46838cae03d
Cushnie <cushniecolle@seattleu.edu>
2019-03-09 14:05:04 -0800

    added main.js file

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const data = [{
+        taxonomy: "Zalophus californiaus",
+        name: "California Sea Lion"
+    },
+    {
+        taxonomy: "Eumetopias jubatus",
+        name: "Steller Sea Lion"
+    },
+    {
+        taxonomy: "Phoca vitulina richardii",
+        name: "Pacific Harbor Seal"
+    }
+]
+// for (pinniped in data) {
+//     let elems = document.querySelectorAll("article .taxonomy")
+//     for (elem in elems) {
+//         elem.innerHTML = pinniped.taxonomy
+//     }
+// }
+let elems = document.querySelectorAll("article");
+for (let i = 0; i < data.length; i++) {
+    let pinniped = data[i];
+    let elem = elems[i];
+    let nameelem = elem.querySelector('.name');
+    if (nameelem) {
+        nameelem.innerHTML = pinniped.name;
+    }
+    let taxonomyelem = elem.querySelector('.taxonomy');
+    if (taxonomyelem) {
+        taxonomyelem.innerHTML = pinniped.taxonomy;
+    }
+}
+
+
+// let actionelems = document.querySelectorAll(".fa-bars");
+// for (let action of actionelems) {
+//     action.addEventListener("click", function (event) {
+        let heartelems = document.querySelectorAll(".fa-heart");
+        for (heart of heartelems) {
+            heart.addEventListener("click", function (event) {
+                alert("Arp! Arp! Arp!");
+            })
+        }
+//     })
+// }
