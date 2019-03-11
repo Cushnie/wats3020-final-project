9db48e7b8fcc523d3cd0f5a894ba9260562d9588
Cushnie <cushniecolle@seattleu.edu>
2019-03-10 21:23:39 -0700

    added all images

--- js/main.js
+++ js/main.js
@@ -71,34 +71,25 @@ const data = [{
         taxonomy: "Aechmophorus occidentalis"
     }
 ]
-// for (pinniped in data) {
-//     let elems = document.querySelectorAll("article .taxonomy")
-//     for (elem in elems) {
-//         elem.innerHTML = pinniped.taxonomy
-//     }
-// }
+
 let elems = document.querySelectorAll("article");
 for (let i = 0; i < data.length; i++) {
-    let pinniped = data[i];
+    let marinebio = data[i];
     let elem = elems[i];
     let nameelem = elem.querySelector('.name');
     if (nameelem) {
-        nameelem.innerHTML = pinniped.name;
+        nameelem.innerHTML = marinebio.name;
     }
     let taxonomyelem = elem.querySelector('.taxonomy');
     if (taxonomyelem) {
-        taxonomyelem.innerHTML = pinniped.taxonomy;
+        taxonomyelem.innerHTML = marinebio.taxonomy;
     }
 }
 
-// let actionelems = document.querySelectorAll(".fa-bars");
-// for (let action of actionelems) {
-//     action.addEventListener("click", function (event) {
+
 let heartelems = document.querySelectorAll(".fa-heart");
 for (heart of heartelems) {
     heart.addEventListener("click", function (event) {
         alert("Hi friend!");
     })
 }
-//     })
-// }
